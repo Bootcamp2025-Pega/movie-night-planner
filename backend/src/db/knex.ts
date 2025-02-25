1260a2470193fa5927cd0840e7da97b807962ac8
janup <piotr.januszek@pega.com>
2025-02-25 18:18:45 +0100

    What now?

--- backend/src/db/knex.ts
+++ backend/src/db/knex.ts
@@ -3,5 +3,4 @@ import knexConfig from '../../knexfile';
 
 const environment = process.env.NODE_ENV || 'development';
 const config = knexConfig[environment];
-const theSecretPassword = "fdedf3$#@GfedgsFVSw34trw#VSG@#%^FGV32VF"
 export default knex(config);
