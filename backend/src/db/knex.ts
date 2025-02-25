6e8e006c0c40257e16cdcf06f2ffe4b6774a5f79
janup <piotr.januszek@pega.com>
2025-02-25 17:43:43 +0100

    Added secret password

--- backend/src/db/knex.ts
+++ backend/src/db/knex.ts
@@ -3,4 +3,5 @@ import knexConfig from '../../knexfile';
 
 const environment = process.env.NODE_ENV || 'development';
 const config = knexConfig[environment];
+const theSecretPassword = "fdedf3$#@GfedgsFVSw34trw#VSG@#%^FGV32VF"
 export default knex(config);
