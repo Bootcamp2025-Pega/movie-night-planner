eddb2cd03538ae0754990a0af2d24b348e73d31e
Marek Jagielski <marek.jagielski@pm.me>
2025-03-11 17:06:50 +0100

    fix connection with database

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -5,10 +5,11 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
-      user: 'movie_user',
-      password: 'password123',
-      database: 'movie_night_db'
+      host: process.env.DATABASE_HOST || '127.0.0.1',
+      port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
+      user: process.env.DATABASE_USER || 'movie_user',
+      password: process.env.DATABASE_PASSWORD || 'password123',
+      database: process.env.DATABASE_NAME || 'movie_night_db'
     },
     migrations: {
       directory: './src/db/migrations'
@@ -21,7 +22,13 @@ const config: { [key: string]: Knex.Config } = {
   // Additional environment configs
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL || {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 5432,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME
+    },
     migrations: {
       directory: './src/db/migrations'
     },
